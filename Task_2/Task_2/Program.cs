d1b731076952330b89db7d64a08c0344431ab421
Eger37 <dan37sugak@gmail.com>
2020-10-09 18:27:07 +0300

    Task_2

--- Task_2/Task_2/Program.cs
+++ Task_2/Task_2/Program.cs
@@ -6,11 +6,23 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
-            Console.WriteLine("Hello!");
-            Console.WriteLine("Hello!");
+            try
+            {
+                Console.Write("Введите переменную типа int: ");
+                int a = Convert.ToInt32(Console.ReadLine());
 
-            //ssss
+                Console.Write("Введите переменную типа double: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите переменную типа long: ");
+                long c = Convert.ToInt64(Console.ReadLine());
+
+                Console.Write($"a = {a}; b = {b}; с = {c}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Exception");
+            }
         }
     }
 }
