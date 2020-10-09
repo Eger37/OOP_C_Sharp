994a47e7ef61aa85ea037d65363a1fd96ece8ed8
Eger37 <dan37sugak@gmail.com>
2020-10-09 18:49:29 +0300

    Task_3

---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите переменную типа int: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Введите переменную типа double: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Введите переменную типа long: ");
+                    long c = Convert.ToInt64(Console.ReadLine());
+
+                    Console.Write($"a = {a}; b = {b}; с = {c}");
+
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Format Exception");
+                    Console.WriteLine("Ведите корректные данные!");
+                }
+            }
+
+
+        }
+    }
+}
