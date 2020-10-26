d53f220af661eb54e38d66cbb9d11fb0d43e1634
Eger37 <dan37sugak@gmail.com>
2020-10-26 11:25:22 +0200

    opppp

---
+++ Oppppop/Oppppop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Oppppop
+{
+    class Program
+    {
+        static int ExampleParamDefault(int x, int y, int z = 5, int s = 7)
+        {
+            return x + y + z + s;
+        }
+        static void Main(string[] args)
+        {
+            ExampleParamDefault(y: 2, x: 3, s: 10);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,17 +6,7 @@ namespace Test
     class Program
     {
         static void Main(string[] args)
-        {
-            //string[] words = { "noob", "friend" };
-            //string[] massiv = { "I", "friend", "am", "a", "noob", "guy" };
-            //massiv = massiv.Except(words).ToArray();
-            //foreach (var word in massiv)
-            //    Console.WriteLine(word);
-
-            string[] lineArr = { "24", "4", "4" ,"4 ", "4 ", " 5", "6", "7", "842", "9", "9", "88,2", "4", "4", "5",  "7", "6", "7", "9", "9" };
-
-            double[] DoubleArr = Array.ConvertAll(lineArr, double.Parse);
-            Console.WriteLine(string.Join(" ", DoubleArr));
+        {            Console.WriteLine());
             //double[] arr1 = { 2.3, 4.2, 4.55 };
             //double[] arr2 = { 5.3, 5.2, 5.55 };
             //double[] arr3 = { 3, 3, 3 };
